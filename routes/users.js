0783cc7f1c9a3eda2b7fd6fe27128a6dab44c354
KULDEEP YADAV <ky2903@gmail.com>
2024-06-10 22:09:39 +0530

    databse setup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const plm =require('passport-local-mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+mongoose.connect("mongodb+srv://kuldeepyadav:@kuldeep@7817992569@cluster0.zhaqliq.mongodb.net/pinterest?retryWrites=true&w=majority&appName=Cluster0");
 
 const userSchema = new mongoose.Schema({
   username: {
